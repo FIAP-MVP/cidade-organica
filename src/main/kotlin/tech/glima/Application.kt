0223c62700a4e2f5784f5703505a5fc8aa3a3aff
Gustavo Lima <gustavosnl@gmail.com>
2022-09-16 19:32:45 -0300

    commit inicial

---
+++ src/main/kotlin/tech/glima/Application.kt
@@ -0,0 +1,12 @@
+package tech.glima
+
+import io.ktor.server.application.*
+import tech.glima.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+}
