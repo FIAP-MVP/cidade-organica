8ca403b8b8f4b70a99acf02c639775d79b7ce4aa
Gustavo Lima <gustavosnl@gmail.com>
2022-09-18 17:41:42 -0300

    Adiciona Kotest e Mockk ao projeto

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,15 @@
+import org.gradle.internal.impldep.org.fusesource.jansi.AnsiRenderer.test
+
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val kotest_version = "5.4.2"
+val mockk_version = "1.12.8"
 plugins {
     application
     kotlin("jvm") version "1.7.10"
     id("io.ktor.plugin") version "2.1.1"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.7.10"
-
 }
 
 group = "tech.glima"
@@ -16,6 +19,10 @@ application {
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
 }
 
 repositories {
@@ -34,4 +41,7 @@ dependencies {
 
     testImplementation("io.ktor:ktor-server-test-host:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("io.kotest:kotest-runner-junit5:$kotest_version")
+    testImplementation("io.kotest:kotest-assertions-core:$kotest_version")
+    testImplementation("io.mockk:mockk:$mockk_version")
 }
