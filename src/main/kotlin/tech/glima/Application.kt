0e016db6c609c934492581285438bcc75e2fedf2
Gustavo Lima <gustavosnl@gmail.com>
2022-09-20 23:24:51 -0300

    Inicializa KOIIN no launch da aplicação

--- src/main/kotlin/tech/glima/Application.kt
+++ src/main/kotlin/tech/glima/Application.kt
@@ -2,14 +2,16 @@ package tech.glima
 
 import io.ktor.server.application.*
 import tech.glima.database.dao.DatabaseFactory
+import tech.glima.di.startKoin
 import tech.glima.plugins.*
 
 fun main(args: Array<String>) = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
-fun Application.module() {
+fun Application.main() {
+    startKoin()
     DatabaseFactory.init()
     configureRouting()
     configureSerialization()
-
 }
+
