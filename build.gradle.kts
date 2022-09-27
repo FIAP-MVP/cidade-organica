f08c1bc58fc742e110be14d61cf3d73fd62aeb19
Gustavo Lima <gustavosnl@gmail.com>
2022-09-26 21:24:50 -0300

    Remove gradlew do gitignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.gradle.internal.impldep.org.fusesource.jansi.AnsiRenderer.test
-
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
